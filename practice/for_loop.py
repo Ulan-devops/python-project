4188d60b11c78738442a2859df7a2fefa0f29928
azaa1 <azaa.chi11@gmail.com>
2019-06-12 16:15:27 -0500

    practice folder added

---
+++ practice/for_loop.py
@@ -0,0 +1,12 @@
+# Running for_loop against a list
+my_list = ['a' , 'b', 'c']
+
+for i in my_list:
+    print (i)
+
+
+# Running for_loop against a dictionary
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+for (key , value) in my_dict.items():
+    print(key, " :: ", value)
